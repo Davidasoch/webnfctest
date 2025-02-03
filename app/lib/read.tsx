d9719cde44a1d745138814887186583a8835e134
Davidasoch <dramleos@gmail.com>
2025-02-03 12:55:30 +0100

    try read

---
+++ app/lib/read.tsx
@@ -0,0 +1,17 @@
+
+
+export  function read() {
+  
+    const ndef = new NDEFReader();
+
+  return new Promise((resolve, reject) => {
+    const ctlr = new AbortController();
+    ctlr.signal.onabort = reject;
+    ndef.addEventListener("reading", event => {
+      ctlr.abort();
+      resolve(event);
+    }, { once: true });
+    ndef.scan({ signal: ctlr.signal }).catch(err => reject(err));
+  });
+}
+
