8b66ce9f3e5578756a1a785ae521e73b42eb3694
Davidasoch <dramleos@gmail.com>
2025-02-04 13:02:06 +0100

    read test

---
+++ hooks/read.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useState, useEffect } from 'react'
+
+export function Read(){
+    const [value, setvalue] = useState(0)
+
+async function Checking(){
+    try{
+        const ndef = new NDEFReader();
+        await ndef.scan();
+        ndef.addEventListener("readingerror", () => {
+          console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+        });
+    
+        ndef.addEventListener("reading", ({ message, serialNumber }) => {
+            setvalue(serialNumber)
+          console.log(`> Serial Number: ${serialNumber}`);
+          console.log(`> Records: (${message.records.length})`);
+        });
+      } catch (error) {
+        console.log("Argh! " + error);
+      } 
+}
+
+    return(
+        <div>
+        <p>{value}</p>
+        <button onClick={() => Checking()}>scan</button>
+    </div>
+      )
+}
