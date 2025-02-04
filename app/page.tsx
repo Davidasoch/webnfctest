176a3f9317fb910387ee4028e89a74b2c30adc6b
Davidasoch <dramleos@gmail.com>
2025-02-04 13:03:10 +0100

    read test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-import { Counter } from "@/hooks/count";
+import { Read } from "@/hooks/read";
 
 
 export default function page() {
@@ -7,7 +6,7 @@ export default function page() {
   
   return (
     <div>
-      <Counter/>
+      <Read/>
     </div>
 
   );
