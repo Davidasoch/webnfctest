a3a97ef444e15ec16a91ed0841e91f7fe8032b68
Davidasoch <dramleos@gmail.com>
2025-02-09 13:24:26 +0100

    test

--- app/hooks/reader.tsx
+++ app/hooks/reader.tsx
@@ -12,7 +12,7 @@ const Scan = () => {
 
     const scan = useCallback(async() => {
 
-        if ('NDEFReader' in window) { 
+        if ('NDEFReader' in window && actions.scan==='scanned') { 
             console.log(actions.scan)
                 try {
                     const ndef = new window.NDEFReader();
